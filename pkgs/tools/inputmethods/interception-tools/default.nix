ae3da5ae6d93acd51a762bd55a5573b6c5b28b9e
Hoàng Minh Thắng <github@banphim.net>
2021-06-06 13:26:02 +0700

    interception-tools: 0.2.1 -> 0.6.6
    - update download url
    - add boost to build inputs
    - remove outdated patches

--- pkgs/tools/inputmethods/interception-tools/default.nix
+++ pkgs/tools/inputmethods/interception-tools/default.nix
@@ -2,26 +2,24 @@
   libevdev, udev }:
 
 let
-  version = "0.2.1";
+  version = "0.6.6";
   baseName = "interception-tools";
 in stdenv.mkDerivation {
   name = "${baseName}-${version}";
 
   src = fetchurl {
-    url = "https://gitlab.com/interception/linux/tools/repository/v${version}/archive.tar.gz";
-    sha256 = "0lqz89wsf9r5xdgflincysxg4l8fpgg5z8zczhhrg9s5787srfzi";
+    url = "https://gitlab.com/interception/linux/tools/-/archive/v${version}/tools-v${version}.tar.gz";
+    sha256 = "1vmg53vagh3m2q96n8jviwmm5gp7jm7iispp43v4wlf715qyn5v9";
   };
 
   nativeBuildInputs = [ cmake pkg-config ];
-  buildInputs = [ libevdev udev libyamlcpp ];
+  buildInputs = [ libevdev udev libyamlcpp boost];
 
   prePatch = ''
     substituteInPlace CMakeLists.txt --replace \
       '"/usr/include/libevdev-1.0"' \
       "\"$(pkg-config --cflags libevdev | cut -c 3-)\""
   '';
-
-  patches = [ ./fix-udevmon-configuration-job-path.patch ];
 
   meta = {
     description = "A minimal composable infrastructure on top of libudev and libevdev";
